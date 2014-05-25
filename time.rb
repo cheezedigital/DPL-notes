90c9205e5e51e54319ec1dd0193db6df6fadab4d
justin veirs <justinveirs@gmail.com>
2014-05-25 09:07:25 -0600

    time

--- time.rb
+++ time.rb
@@ -14,9 +14,28 @@ birds = {"box 1" => "stuff", "box 2" => "stuff"}
 
 puts ["box 1"]
 =end
-
+=begin
 a = 2
 
 if a == 2
   puts "I am happy I finally figured something out"
 end
+=end
+prints = "what's your first name?"
+first_name = gets.chomp
+first_name.capitalize
+
+prints = "what's your last name?"
+last_name = gets.chomp
+last_name.capitalize
+
+prints = "what city are you from?"
+city = gets.chomp
+city.capitalize
+
+prints = "what state are you from?"
+state = gets.chomp
+state.upcase
+
+puts = "your name is #{first_name} and #{last_name} is,
+        and you're from #{city}, #{state}!"
