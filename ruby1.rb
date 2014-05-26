5155e9bbcdb8ed4eeb281d41517128e841f55dc7
justin veirs <justinveirs@gmail.com>
2014-05-26 08:52:08 -0600

    added another file for some notes

---
+++ ruby1.rb
@@ -0,0 +1,91 @@
+=begin
+if 1 > 2
+  print "player 1 goes first"
+else
+  print "player 2 goes first"
+end
+=end
+=begin
+if 1 > 2
+  print "player 1 goes first"
+elsif 1 < 2
+  print "two is greater than 1"
+else
+  print "player 2 goes first"
+end
+=end
+=begin
+puts 1.0 + 2.0
+puts 2.0 * 3.0
+puts 5.0 - 8.0
+puts 9.0 / 2.0
+
+var1 = 2
+var2 = '5'
+puts var1.to_s + var2
+
+puts "15".to_f
+puts "99.999".to_f
+puts "99.999".to_i
+''
+"5 is my favorite number!".to_i
+"who asked you about 5 or whatever?".to_i
+"your momma did.".to_f
+''
+"stringt".to_s
+3.to_1
+=end
+=begin
+puts "hello there, and what is your name?"
+name = gets.chomp
+puts "your name is " + name + "? what a lovely name!"
+puts "pleased to meet you"
+=end
+
+=begin
+name = "Justin Elliott Veirs"
+puts "hello there, and what is your full name?"
+puts "#{name.chomp}"
+=end
+
+=begin
+puts "what's your full name?"
+name = gets.chomp
+puts "did you know there are " + name.length.to_s + " characters"
+puts "in your name, " + name + "?"
+=end
+
+=begin
+letters = "aAbBcCDdEe"
+puts letters.upcase
+puts letters.downcase
+puts letters.swapcase
+puts letters.capitalize
+puts ' a'.capitalize
+puts letters
+=end
+
+=begin
+puts "hello, What\'s your name?"
+name = gets.chomp
+puts "hello, " + name + "."
+
+if name == "Justin"
+  puts "what a typical merican name"
+end
+=end
+
+puts "here\'s the store of the three little pigs with one pig having a straw house,"
+puts "the other had a wood house, and finally, the last pig having a brick house!"
+
+name = ["straw", "wood", "brick"]
+
+if name == "straw"
+  puts "give me some food, otherwise, the wolf will blow your house down"
+  puts "but I don't have any food"
+elsif name = "wood"
+  puts "I just blew down your bros abode, now give me some food,"
+  puts "or I'll blow your house down!"
+else
+
+end
